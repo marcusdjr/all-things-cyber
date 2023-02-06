ff09131b13316df4891dc52a000995617daf5dd0
marcusdjr <31329300+marcusdjr@users.noreply.github.com>
2023-02-06 18:05:52 -0500

    configuring pipline

--- pipeline/scripts/get.py
+++ pipeline/scripts/get.py
@@ -1,11 +1,18 @@
 # ---
 # jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.4
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
+# %%
 # Add description here
 #
 # *Note:* You can open this file as a notebook (JupyterLab: right-click on it in the side bar -> Open With -> Notebook)
